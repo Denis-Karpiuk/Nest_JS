7791ce5003c5f92187ee46f0d10a2a585c1ab81a
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2026-01-29 01:13:52 +0300

    lesson_15: fix create blog post

--- src/modules/bloggers-platform/modules/blogs/api/input-dto/creat-blog-post.dto.ts
+++ src/modules/bloggers-platform/modules/blogs/api/input-dto/creat-blog-post.dto.ts
@@ -1,5 +1,18 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { IsStringWithTrim } from 'src/core/decorators/validation/is-string-with-trim';
 export class CreateBlogPostDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsStringWithTrim(1, 30)
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStringWithTrim(1, 100)
   shortDescription: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStringWithTrim(1, 1000)
   content: string;
 }
