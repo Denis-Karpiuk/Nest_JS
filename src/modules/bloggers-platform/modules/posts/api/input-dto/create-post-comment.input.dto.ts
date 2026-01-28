53de16bf61b00effc34618b801776c61df609b14
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2026-01-29 00:18:35 +0300

    lesson_15: fix

--- src/modules/bloggers-platform/modules/posts/api/input-dto/create-post-comment.input.dto.ts
+++ src/modules/bloggers-platform/modules/posts/api/input-dto/create-post-comment.input.dto.ts
@@ -1,3 +1,9 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { IsStringWithTrim } from 'src/core/decorators/validation/is-string-with-trim';
+
 export class CreatePostCommentInputDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsStringWithTrim(20, 300)
   content: string;
 }
--- src/modules/bloggers-platform/modules/posts/api/input-dto/create-post.input.dto.ts
+++ src/modules/bloggers-platform/modules/posts/api/input-dto/create-post.input.dto.ts
@@ -1,7 +1,23 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { IsStringWithTrim } from 'src/core/decorators/validation/is-string-with-trim';
+
 export class CreatePostInputDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsStringWithTrim(1, 30)
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStringWithTrim(1, 100)
   shortDescription: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStringWithTrim(1, 1000)
   content: string;
+
+  @IsNotEmpty()
   blogId: string;
 }
 
