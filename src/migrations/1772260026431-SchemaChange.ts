36d4ee672f745359e2494e8ac77935c12c8e5ba0
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2026-02-28 09:29:45 +0300

    lesson_23: migrations

--- src/migrations/1772260026431-SchemaChange.ts
+++ src/migrations/1772260026431-SchemaChange.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class SchemaChange1772260026431 implements MigrationInterface {
-    name = 'SchemaChange1772260026431'
+  name = 'SchemaChange1772260026431';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "some" boolean NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "some" boolean NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "some"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "some"`);
+  }
 }
