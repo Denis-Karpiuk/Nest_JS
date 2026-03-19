4baa09727c490c6e6fb25f68d90d41ffe4d1a83a
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2026-03-19 15:37:52 +0300

    lesson_25: fix game and palyer dto

--- src/modules/quiz/modules/games/api/view-dto/game.view-dto.ts
+++ src/modules/quiz/modules/games/api/view-dto/game.view-dto.ts
@@ -2,29 +2,33 @@ import { Game } from '../../domain/game.entity';
 import { PlayerViewDto } from './player.view-dto';
 import { QuestionViewDto } from './question.view-dto';
 import { Question } from '../../../questions/domain/question.entity';
+import { GameStatus } from '../../domain/dto/create-game.dto';
 
 export class GameViewDto {
   id: string;
   firstPlayerProgress: PlayerViewDto;
   secondPlayerProgress: PlayerViewDto | null;
-  questions: QuestionViewDto[];
+  questions: QuestionViewDto[] | null;
   status: string;
   pairCreatedDate: Date;
-  startGameDate: Date;
-  finishGameDate: Date;
-  player: PlayerViewDto;
+  startGameDate: Date | null;
+  finishGameDate: Date | null;
 
   static mapToView(game: Game, questions: Question[]): GameViewDto {
     const dto = new GameViewDto();
-    console.log(game);
     dto.id = game.id;
     dto.status = game.status;
-    dto.questions = questions?.map(QuestionViewDto.mapToView);
     dto.pairCreatedDate = game.createdAt;
+    dto.startGameDate = game.startDate ?? null;
+    dto.finishGameDate = game.finishDate ?? null;
     dto.firstPlayerProgress = PlayerViewDto.mapToView(game.firstPlayer);
     dto.secondPlayerProgress = game.secondPlayer
       ? PlayerViewDto.mapToView(game.secondPlayer)
       : null;
+    dto.questions =
+      game.status === GameStatus.PendingSecondPlayer
+        ? null
+        : (questions?.map(QuestionViewDto.mapToView) ?? []);
 
     return dto;
   }
